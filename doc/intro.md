7326c980848e6c22b9f74c559acdb576c67405bf
asso <asso@topsoe.com>
2019-05-26 09:22:08 +0530

    Initialized template.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adlic
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.versoni/adlic "1.0.0"
+  :description "Azure datalake genreration 2 in clojure."
+  :url "http://example.com/FIXME"
+  :license {:name ""
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns adlic.core})
